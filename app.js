9019b8c4c4b3a5604123b6cf5bf991067f82b061
Natchanan Lordee <natchanan.lor@gmail.com>
2025-01-23 22:36:20 +0700

    Update main

--- app.js
+++ app.js
@@ -1,36 +1,34 @@
 const express = require("express");
 const app = express();
-const multer = require('multer');
-const path = require('path')
+const multer = require("multer");
+const path = require("path");
 const vision = require("@google-cloud/vision");
-const {Pool} = require("pg");
+const { Pool } = require("pg");
 const bcrypt = require("bcrypt");
 const port = 3000;
 
 const pool = new Pool({
-  user:"postgres",
-  host:"localhost",
-  database:"myapp",
-  password:"admin",
-  port:5432,
-})
-
-
+  user: "postgres",
+  host: "localhost",
+  database: "myapp",
+  password: "admin",
+  port: 5432,
+});
 
 const client = new vision.ImageAnnotatorClient({
   keyFilename: "subtle-bus-443807-m7-30bc6e8c9240.json",
 });
 
 const storage = multer.diskStorage({
-  destination:(req,file,cb) => {
-    cb(null,'uploads/');
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
   },
-  filename:(req,file,cb) =>{
-    cb(null,Date.now()+path.extname(file.originalname));
-  }
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
 
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });
 const formUpload = multer();
 
 async function detectLabels(imagePath) {
@@ -45,12 +43,11 @@ async function detectLabels(imagePath) {
     console.error("Error detecting labels:", error);
   }
 }
+
 async function hashPassword(password) {
   const saltRounds = 10;
-  const hashPassword = await bcrypt.hash(password,saltRounds);
+  const hashPassword = await bcrypt.hash(password, saltRounds);
   return hashPassword;
- 
-  
 }
 
 /*async function hashidentityCard(identitynumber) {
@@ -61,87 +58,88 @@ async function hashPassword(password) {
 
 app.use(express.json());
 
-pool.connect()
+pool
+  .connect()
   .then(() => console.log("connected to PostgreSQL"))
-  .catch((error) => console.error("Failed",error));
+  .catch((error) => console.error("Failed", error));
 
-app.get('/users',async(req,res) =>{
+app.get("/users", async (req, res) => {
   const result = await pool.query("SELECT * FROM users");
   res.json(result.rows);
 });
 
-app.post("/users",async(req,res) =>{
-  const {name,email} = req.body;
-  console.log(name,email);
-  const result = await pool.query("INSERT INTO users(name,email) VALUES($1,$2) RETURNING *",
-    [name,email]
+app.post("/users", async (req, res) => {
+  const { name, email } = req.body;
+  console.log(name, email);
+  const result = await pool.query(
+    "INSERT INTO users(name,email) VALUES($1,$2) RETURNING *",
+    [name, email]
   );
-
   res.json(result.rows[0]);
+});
 
-});
-app.post('/api/register',formUpload.none(),async(req,res) => {
-  const{identitynumber,password,fullname,dateofbirth} = req.body;
-  try{
+app.post("/api/register", formUpload.none(), async (req, res) => {
+  const { identitynumber, password, fullname, dateofbirth } = req.body;
+  try {
     const hashedPassword = await hashPassword(password);
-    const result = await pool.query("INSERT INTO users(identitycard,password,fullname,dateofbirth) VALUES($1,$2,$3,$4) RETURNING *",
-    [identitynumber,hashedPassword,fullname,dateofbirth]
+    const result = await pool.query(
+      "INSERT INTO users(identitycard,password,fullname,dateofbirth) VALUES($1,$2,$3,$4) RETURNING *",
+      [identitynumber, hashedPassword, fullname, dateofbirth]
     );
-    res.json({status: true});
+    res.json({ status: true });
     console.log(result.rows[0]);
-  }catch(error){
-    console.error('Error:',error);
-    res.json({status: false});
+  } catch (error) {
+    console.error("Error:", error);
+    res.json({ status: false });
   }
-  
 });
-app.post('/api/login', formUpload.none(), async (req,res) => {
-  const { identitynumber , password} = req.body; 
+
+app.post("/api/login", formUpload.none(), async (req, res) => {
+  const { identitynumber, password } = req.body;
   if (!identitynumber || !password) {
-    return res.status(400).json({ 
-      message: "Missing required parameters", 
-      required: ["identitynumber", "password"] 
+    return res.status(400).json({
+      message: "Missing required parameters",
+      required: ["identitynumber", "password"],
     });
   }
   try {
-    const result = await pool.query(`SELECT * FROM users WHERE identitycard = $1`, [identitynumber]);
+    const result = await pool.query(
+      `SELECT * FROM users WHERE identitycard = $1`,
+      [identitynumber]
+    );
     console.log(result.rows[0]);
     const user = result.rows[0];
-    const isPasswordValid = await bcrypt.compare(password,user.password);
-    if(isPasswordValid){
-      res.json({status:true,fullname:user.fullname});
-    }else{
-      res.json({status:false})
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid) {
+      res.json({ status: true, fullname: user.fullname });
+    } else {
+      res.json({ status: false });
     }
-    
-  } catch (error){
+  } catch (error) {
     console.log(error);
-    res.json({status: false});
+    res.json({ status: false });
   }
-  
-  
 });
 
-app.post('/api/upload',upload.single('image'),async(req,res) => {
+app.post("/api/upload", upload.single("image"), async (req, res) => {
   console.log(req.file);
-  if(!req.file){
-    return res.status(400).send({message:'No file uploaded'});
+  if (!req.file) {
+    return res.status(400).send({ message: "No file uploaded" });
   }
   const filename = req.file.filename;
-  const  textOcr = await detectLabels(`uploads/${filename}`);
- 
-  res.json({message:`${textOcr}`});
+  const textOcr = await detectLabels(`uploads/${filename}`);
+
+  res.json({ message: `${textOcr}` });
 });
 
 app.get("/", (req, res) => {
   res.json({ name: "Natchanan Lordee" });
 });
 
-app.get("/api/ocr", async(req, res) => {
-  drug_name = await detectLabels('sample_medicine_label_02.jpg');
-  res.json({ message:`${drug_name}`})
-  console.log(1)
-
+app.get("/api/ocr", async (req, res) => {
+  drug_name = await detectLabels("sample_medicine_label_02.jpg");
+  res.json({ message: `${drug_name}` });
+  console.log(1);
 });
 
 app.listen(port, () => {
