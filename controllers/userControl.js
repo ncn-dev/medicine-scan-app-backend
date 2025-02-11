2dff91bf97c073c7cb2011932da4b62e1948d81a
Natchanan Lordee <natchanan.lor@gmail.com>
2025-02-12 02:11:58 +0700

    create useRoute.js

--- controllers/userControl.js
+++ controllers/userControl.js
@@ -1,7 +1,27 @@
 const pool = require("../models/db")
+
 exports.getmedbag = async (req, res) => {
     const { idcard } = req.params;
     const result = await pool.query("SELECT * FROM images WHERE idcard = $1",[idcard]);
    
     res.status(200).json(result.rows);
+};
+
+exports.deletemedbag = async (req,res) => {
+    const { idcard } = req.params;
+   
+    console.log(idcard)
+    try{
+        const result = await pool.query("DELETE FROM images WHERE id = $1",[idcard]);
+        
+        if(result.rowCount == 0){
+            return res.status(404).json({message:"Item not found"});
+        }
+
+        res.status(200).json({message: "Item deleted successfully"});
+
+    }catch(error){
+        console.error(error.message);
+        res.status(500).json({message: "Server error"});
+    }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const router = express.Router()
 const { getmedbag } = require("../controllers/userControl")
+const {deletemedbag} = require("../controllers/userControl")
+
 router.get("/user/medbag/:idcard", getmedbag)
+
+router.post("/user/deletemedbag/:idcard", deletemedbag)
 
 module.exports = router
